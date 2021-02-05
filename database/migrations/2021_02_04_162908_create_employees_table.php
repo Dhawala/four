677321fc13c35d549fdb4b9736b7949afebd0043
Dhawala Ranaeera <1234dhawala@gmail.com>
2021-02-05 16:46:21 +0530

    employee migration

--- database/migrations/2021_02_04_162908_create_employees_table.php
+++ database/migrations/2021_02_04_162908_create_employees_table.php
@@ -15,6 +15,25 @@ class CreateEmployeesTable extends Migration
     {
         Schema::create('employees', function (Blueprint $table) {
             $table->id();
+            $table->string('last_name',60);
+            $table->string('first_name', 6);
+            $table->string('middle_name',60)->nullable();
+            $table->string('address', 120);
+            $table->integer('department_id')->unsigned();
+            $table->foreign('department_id')->references('id')->on('departments')
+                ->onDelete('cascade')->onUpdate('cascade');
+            $table->integer('city_id')->unsigned();
+            $table->foreign('city_id')->references('id')->on('cities')
+                ->onDelete('cascade')->onUpdate('cascade');
+            $table->integer("state_id")->unsigned();
+            $table->foreign('state_id')->references('id')->on('states')
+                ->onDelete('cascade')->onUpdate('cascade');
+            $table->integer('country_id')->unsigned();
+            $table->foreign('country_id')->references('id')->on('country')
+                ->onDelete('cascade')->onUpdate('cascade');
+            $table->char('zip', 10);
+            $table->date('birthdate');
+            $table->date('date_hired');
             $table->timestamps();
             $table->softDeletes();
         });
